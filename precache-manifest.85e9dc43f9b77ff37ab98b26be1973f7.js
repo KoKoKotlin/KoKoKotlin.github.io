e9aa0ef7d2d7932e56efed7497899e19f4f8b371
Yannik Höll <labruzzler@gmail.com>
2020-09-28 12:18:38 +0200

    Updates

---
+++ precache-manifest.85e9dc43f9b77ff37ab98b26be1973f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530b74996d9022a879d73559bbb859d6",
+    "url": "/KoKoKotlin.github.io/index.html"
+  },
+  {
+    "revision": "728c283ee22ccd5d81fe",
+    "url": "/KoKoKotlin.github.io/static/css/main.9e99889c.chunk.css"
+  },
+  {
+    "revision": "70ab0f055886dc8d2a4d",
+    "url": "/KoKoKotlin.github.io/static/js/2.fbb011c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KoKoKotlin.github.io/static/js/2.fbb011c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "728c283ee22ccd5d81fe",
+    "url": "/KoKoKotlin.github.io/static/js/main.ea42d415.chunk.js"
+  },
+  {
+    "revision": "f4ade8ba89742726e671",
+    "url": "/KoKoKotlin.github.io/static/js/runtime-main.5e4d5de5.js"
+  }
+]);
